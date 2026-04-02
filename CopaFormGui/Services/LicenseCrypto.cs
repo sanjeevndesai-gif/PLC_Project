95eefafdac38bfd18c14a24f36c71fc009bcc7b1
sanjeevndesai-gif <sanjeev.n.desai@gmail.com>
2026-04-03 01:27:59 +0530

    Rotate license keys and update public key for validation

--- CopaFormGui/Services/LicenseCrypto.cs
+++ CopaFormGui/Services/LicenseCrypto.cs
@@ -8,15 +8,15 @@ public static class LicenseCrypto
     // Replace with your production public key in PEM format.
     // Keep the matching private key only inside your dev licensing tool/workstation.
     public const string PublicKeyPem = @"-----BEGIN PUBLIC KEY-----
-MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAyP+JzIjxR8SYDaFNqoCj
-lk0pFivJlSZFNfJJHuZgFYvGY5boQ/UOLGpI/9vZbvHP2HXxOTa24pUmCrm+V7v5
-n7GkdmG/F3BRJ8+PPlXhR8IOnrX9IVTRs5JBy4fPRbOr+SyrrN4m6fWZIB5Q5/mU
-O6XQKBXo/Oj0OdfG+fh/8u5ztE1yQnydNEkWPtuirECx5G53EihOWiHGdzt9XZqB
-QI3+xFlEEupTE/5b+XtXWMn31k5kEX62GfvRxOChAypajpfGr+vuaTCogeJByONs
-9y+qzSKXgq3dHGg7PqnFsHF6HzYHcUVMhM2fTDexTBXvkZ6dsjpIl0gim965wmLc
-PlpkSjRbXwbXea+uoPMt1BVjmUo94s0CJnukmRhXeYG3/6t2bUZBc4rHUZ9hnSg5
-1CgMnjjwwVBfzDHVTZQphAg9FmUtdEJDfF0rjBvJ4bwiIidoVZGFih1x9fDILvXE
-n93IlX+55x865HmtfXWsP09MKUsD6MBSroCHf33gOnMFAgMBAAE=
+MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAmzTxQ7oSVdqgtPfsvYHT
+YCLGYzgfI1z7pMAMoIG95d5y3wzlDvdDY0uUSgRihh2zukSNvdx+z5qTOI3LYZ4O
+7/F3JBYxQP57W/gaVcz7J56+z5P3iIygcMDtavZIq2TLXn9oI9G7MQnkgCmLFKAo
+jFBb+MX1hDTo0yj+723EdfSOnwf7bY2TyFYNIFc/vRvAkKogCdyeRQu7nNtti1Hg
+2hTWkgUbg2PHu8NjroaBp9NS11hYjk1+KZ3BrKgV4fMZM8PSoyX1NqlNcdFBhDk2
+/XvDz/L2KXl5UncXM0ge9LxKj+IWNd9ePjTvJa4jE7gVzH4Y820DdcVdgFrevnKJ
+kP/6ojc4N3oUaAa3BmGF4zEERXfrEg8ljoEYPn1krVb89rJ+dOqT1PxTkqZXmMLw
+3tayl+ZEAeKfDm0JXYCzdWLk48mWVEOjN7EXicFejvEclCb2I1rD1S6N0hQbTqgt
+9x20/+C8tzn9tAsdGHgXcQEa5pNflx1tvVzDWnEQw6gZAgMBAAE=
 -----END PUBLIC KEY-----";
 
     public static string BuildPayload(LicenseFile license)
